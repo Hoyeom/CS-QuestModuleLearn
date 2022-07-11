256c2d44e687d67575cf0c15f872aa0277b631c8
Hoyeom <korehdrb@gmail.com>
2022-07-11 20:17:00 +0900

    Scriptable Task

---
+++ 3DQuestModuleLearn/Assets/Scripts/Quest/Task/Action/SimpleSet.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quest/Task/Action/SimpleSet",fileName = "Simple Set")]
+public class SimpleSet : TaskAction
+{
+    public override int Run(Task task, int currentSuccess, int successCount)
+    {
+        return successCount;
+    }
+}
